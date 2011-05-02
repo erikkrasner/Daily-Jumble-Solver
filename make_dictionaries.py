c99620f3f2632b3eb3472a5ee587e7d83decdf09
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-05-02 13:41:18 -0300

    dictionary making script takes popularity into account

--- make_dictionaries.py
+++ make_dictionaries.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-import os, sys
+import os, sys, re
 
 for file_name in os.listdir(sys.argv[1]):
     raw_dict_file = open(sys.argv[1] + "/" + file_name,'r')
+    popularity = re.search(r'\d+\Z',file_name).group(0)
     for line in raw_dict_file:
         word = line[:-1]
         letter = sorted(word)[0]
         length = len(word)
         dict_file = open("word_lists/%s%d" % (letter,length),'a')
-        dict_file.write(word + '\n')
+        dict_file.write('%s %s\n' % (word, popularity))
         dict_file.close()
     raw_dict_file.close()
