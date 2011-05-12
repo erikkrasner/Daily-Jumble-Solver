bb968d2db8b96d8fd5d2ed10fd3a36efebb4e439
Erik Krasner-Karpen <erik.krasnerkarpen@gmail.com>
2011-05-12 19:53:03 -0400

    more frontend, ajax on its way

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ import wsgiref.handlers
 from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+#import json
 
 class HomePage(webapp.RequestHandler):
     def get(self):
         html = template.render('home.html',{})
         self.response.out.write(html)
 
-class DisplaySolutions(webapp.RequestHandler):
-    def post(self):
-        html = template.render('display_solutions.html',{})
-        self.response.out.write(html)
+class AjaxHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("hello world")
 
 application = webapp.WSGIApplication([
     ('/',HomePage),
-    ('/display',DisplaySolutions)
+    ('/solve',AjaxHandler)
     ],debug=True)
 
 def main():
